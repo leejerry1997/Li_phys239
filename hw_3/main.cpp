2bdd93a7871840e9fc9f7a5d0a487021096a6832
leejerry1997 <leejerry1997@hotmail.com>
2020-10-27 14:28:06 -0700

    add hw3 directory

---
+++ hw_3/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * HW 3   File name: main.cpp
+ **/
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <valarray>
+#include <sciplot/sciplot.hpp>
+
+using namespace std;
+
+/**
+ * Driver function
+ **/
+int main()
+{
+
+	return 0;
+}
